3ab32171f3f5c4e293518b5f8038c81c7feaab75
jziemer1996 <jz199623@gmail.com>
2022-10-24 11:42:48 +0200

    updated main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+import ascending
 import descending
 import os
 
@@ -7,13 +8,14 @@ def main():
     # Your reformatted output CSV to be located
     working_dir = "D:/Aktuelles/Promotion/Packages/StaMPS_Visualizer-master/input/stusi/"
     output_dir = "D:/Aktuelles/Promotion/Programmierung/R/Stamps2CSV/output/"
-    stamps_csv = "Lister_final_stamps_export_coh_var_disp_ts.csv"
-    output_csv = "Lister_PS_points_ts_R.csv"
+    stamps_csv = "Moehne_asc_final_stamps_export_coh_var_disp_ts.csv"
+    output_csv = "Moehne_Asc_PS_points_ts_R.csv"
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # Executable function
-    descending.columns(working_dir, output_dir, stamps_csv, output_csv)
+    ascending.columns(working_dir, output_dir, stamps_csv, output_csv)
+    # descending.columns(working_dir, output_dir, stamps_csv, output_csv)
 
 
 if __name__ == "__main__":
